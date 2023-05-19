f0dcd49401ad42e93cdfac27ab4463106b4c9c45
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-05-19 14:56:38 +0200

    use flex layout to position group list and router outlet side by side

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <app-toolbar></app-toolbar>
-    <div>
-      <app-group-list></app-group-list>
-      <router-outlet></router-outlet>
+    <div class="fx-row">
+      <div class="fx-flex-30">
+        <app-group-list></app-group-list>
+      </div>
+      <div class="fx-grow">
+        <router-outlet></router-outlet>
+      </div>
     </div>
   `,
+  styleUrls: ['shared/styles/flex.scss'],
   styles: [],
 })
 export class AppComponent {}
