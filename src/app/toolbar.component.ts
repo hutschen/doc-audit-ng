a961b2106abd081a98b1ab3b7895090bda2ee6ad
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-05-19 14:20:20 +0200

    generate ToolbarComponent

--- src/app/toolbar.component.ts
+++ src/app/toolbar.component.ts
@@ -13,17 +13,11 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { Component } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ToolbarComponent } from './toolbar.component';
-
-@NgModule({
-  declarations: [AppComponent, ToolbarComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
-  bootstrap: [AppComponent],
+@Component({
+  selector: 'app-toolbar',
+  template: ` <p>toolbar works!</p> `,
+  styles: [],
 })
-export class AppModule {}
+export class ToolbarComponent {}
