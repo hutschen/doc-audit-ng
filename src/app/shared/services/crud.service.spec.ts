3cd6ee03c1247c6327d3f0396d6e021bc676f230
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-05-19 20:19:15 +0200

    implement CRUDService

--- src/app/shared/services/crud.service.spec.ts
+++ src/app/shared/services/crud.service.spec.ts
@@ -14,18 +14,120 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import { TestBed } from '@angular/core/testing';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { CRUDService } from './crud.service';
+import { environment } from 'src/environments/environment';
+
+interface IItemInput {
+  name: string;
+}
+
+interface IItemOutput extends IItemInput {
+  id: number;
+}
 
 describe('CRUDService', () => {
-  let service: CRUDService;
+  let sut: CRUDService<IItemInput, IItemOutput>;
+  let httpMock: HttpTestingController;
+  let baseUrl: string;
+  let inputMock: IItemInput;
+  let outputMock: IItemOutput;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CRUDService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    sut = TestBed.inject(CRUDService);
+    httpMock = TestBed.inject(HttpTestingController);
+    baseUrl = environment.baseUrl;
+    inputMock = {
+      name: 'A test item',
+    };
+    outputMock = {
+      id: 1,
+      name: inputMock.name,
+    };
   });
 
-  xit('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should be created', () => {
+    expect(sut).toBeTruthy();
+  });
+
+  it('should convert a relative to a absolute url', () => {
+    const relativeUrl = 'this/is/a/test';
+    expect(sut.toAbsoluteUrl(relativeUrl)).toEqual(`${baseUrl}/${relativeUrl}`);
+  });
+
+  it('should query items', (done: DoneFn) => {
+    sut.query('items').subscribe({
+      next: (value) => {
+        if (!Array.isArray(value)) {
+          value = value.items;
+        }
+        expect(value.length).toEqual(1);
+        expect(value[0]).toEqual(outputMock);
+      },
+      complete: () => done(),
+    });
+    const mockResponse = httpMock.expectOne({
+      method: 'get',
+      url: baseUrl + '/items',
+    });
+    mockResponse.flush([outputMock]);
+  });
+
+  it('should create an item', (done: DoneFn) => {
+    sut.create('items', inputMock).subscribe({
+      next: (value) => expect(value).toEqual(outputMock),
+      complete: () => done(),
+    });
+    const mockResponse = httpMock.expectOne({
+      method: 'post',
+      url: baseUrl + '/items',
+    });
+    mockResponse.flush(outputMock);
+  });
+
+  it('should read an item', (done: DoneFn) => {
+    sut.read('items/1').subscribe({
+      next: (value) => expect(value).toEqual(outputMock),
+      complete: () => done(),
+    });
+    const mockResponse = httpMock.expectOne({
+      method: 'get',
+      url: baseUrl + '/items/1',
+    });
+    mockResponse.flush(outputMock);
+  });
+
+  it('should update an item', (done: DoneFn) => {
+    sut.update('items/1', inputMock).subscribe({
+      next: (value) => expect(value).toEqual(outputMock),
+      complete: () => done(),
+    });
+    const mockResponse = httpMock.expectOne({
+      method: 'put',
+      url: baseUrl + '/items/1',
+    });
+    mockResponse.flush(outputMock);
+  });
+
+  it('should delete an item', (done: DoneFn) => {
+    sut.delete('items/1').subscribe({
+      next: (value) => expect(value).toBeNull(),
+      complete: () => done(),
+    });
+    const mockResponse = httpMock.expectOne({
+      method: 'delete',
+      url: baseUrl + '/items/1',
+    });
+    mockResponse.flush(null);
   });
 });
--- src/app/shared/services/crud.service.ts
+++ src/app/shared/services/crud.service.ts
@@ -13,11 +13,79 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+export interface IPage<T> {
+  items: T[];
+  total_count: number;
+}
+
+export interface IQueryParams {
+  [param: string]:
+    | string
+    | number
+    | boolean
+    | ReadonlyArray<string | number | boolean>;
+}
 
 @Injectable({
   providedIn: 'root',
 })
-export class CRUDService {
-  constructor() {}
+export class CRUDService<InputType, OutputType> {
+  constructor(protected _httpClient: HttpClient) {}
+
+  toAbsoluteUrl(relativeUrl: string): string {
+    return `${environment.baseUrl}/${relativeUrl}`;
+  }
+
+  protected get _httpOptions(): object {
+    return {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    };
+  }
+
+  query(
+    relativeUrl: string,
+    params: IQueryParams = {}
+  ): Observable<OutputType[] | IPage<OutputType>> {
+    return this._httpClient.get<OutputType[]>(this.toAbsoluteUrl(relativeUrl), {
+      params,
+      ...this._httpOptions,
+    });
+  }
+
+  create(relativeUrl: string, itemInput: InputType): Observable<OutputType> {
+    return this._httpClient.post<OutputType>(
+      this.toAbsoluteUrl(relativeUrl),
+      itemInput,
+      this._httpOptions
+    );
+  }
+
+  read(relativeUrl: string): Observable<OutputType> {
+    return this._httpClient.get<OutputType>(
+      this.toAbsoluteUrl(relativeUrl),
+      this._httpOptions
+    );
+  }
+
+  update(relativeUrl: string, itemInput: InputType): Observable<OutputType> {
+    return this._httpClient.put<OutputType>(
+      this.toAbsoluteUrl(relativeUrl),
+      itemInput,
+      this._httpOptions
+    );
+  }
+
+  delete(relativeUrl: string): Observable<null> {
+    return this._httpClient.delete<null>(
+      this.toAbsoluteUrl(relativeUrl),
+      this._httpOptions
+    );
+  }
 }
