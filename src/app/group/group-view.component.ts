be5b32726bbed3da059782f3bd6fee1673c81e10
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-05-20 14:15:20 +0200

    get group id from route params in GroupViewComponent

--- src/app/group/group-view.component.ts
+++ src/app/group/group-view.component.ts
@@ -14,6 +14,11 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Group, GroupService } from '../shared/services/group.service';
+import { Observable, catchError, map, of, switchMap } from 'rxjs';
+import { toInt } from 'radash';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-group-view',
@@ -51,4 +56,28 @@ import { Component } from '@angular/core';
     '.icon-button { min-width: 0px; }',
   ],
 })
-export class GroupViewComponent {}
+export class GroupViewComponent {
+  group$: Observable<Group | null>;
+
+  constructor(
+    protected _route: ActivatedRoute,
+    protected _router: Router,
+    protected _groupService: GroupService
+  ) {
+    this.group$ = this._route.paramMap.pipe(
+      map((params) => toInt(params.get('groupId'), null)),
+      switchMap((groupId) => {
+        if (groupId === null) return of(null);
+        return this._groupService.getGroup(groupId);
+      }),
+      catchError((error) => {
+        if (error instanceof HttpErrorResponse && error.status === 404) {
+          this._router.navigate(['/routes']);
+          return of(null);
+        } else {
+          throw error;
+        }
+      })
+    );
+  }
+}
