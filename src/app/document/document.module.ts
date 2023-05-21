1c8d40c19d8d3e1da7e10c8568397ea750b5878b
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-05-21 15:37:31 +0200

    generate DocumentDialogComponent

--- src/app/document/document.module.ts
+++ src/app/document/document.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { DocumentListComponent } from './document-list.component';
 import { MaterialModule } from '../material/material.module';
 import { CreateDocumentComponent } from './create-document.component';
+import { DocumentDialogComponent } from './document-dialog.component';
 
 @NgModule({
-  declarations: [DocumentListComponent, CreateDocumentComponent],
+  declarations: [DocumentListComponent, CreateDocumentComponent, DocumentDialogComponent],
   imports: [CommonModule, MaterialModule],
   exports: [DocumentListComponent],
 })
