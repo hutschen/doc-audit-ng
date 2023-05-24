4de61fc9b5d5d361dfb349db301b81c02021e75f
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2023-05-24 20:38:00 +0200

    implement ErrorDialogComponent

--- src/app/shared/components/error-dialog.component.ts
+++ src/app/shared/components/error-dialog.component.ts
@@ -13,11 +13,49 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import { ErrorReport } from '../services/error.service';
 
 @Component({
   selector: 'app-error-dialog',
-  template: ` <p>error-dialog works!</p> `,
-  styles: [],
+  template: `
+    <div mat-dialog-title>
+      <span *ngIf="1 >= errorReports.length">Error Report</span>
+      <span *ngIf="1 < errorReports.length">Error Reports</span>
+    </div>
+    <div mat-dialog-content>
+      <ul>
+        <li *ngFor="let errReport of errorReports">
+          <strong *ngIf="errReport.code">{{ errReport.code }}&nbsp;</strong>
+          <strong *ngIf="errReport.title">{{ errReport.title }}&nbsp;</strong>
+          <span *ngIf="errReport.detail">{{ errReport.detail }}</span>
+        </li>
+      </ul>
+    </div>
+    <div mat-dialog-actions align="end">
+      <button mat-raised-button color="accent" (click)="onClose()">
+        Close
+      </button>
+    </div>
+  `,
+  styles: [
+    'ul { list-style-type: none; margin: 0; padding: 0; }',
+    'li:not(:last-child) { margin-bottom: 1em; }',
+  ],
 })
-export class ErrorDialogComponent {}
+export class ErrorDialogComponent {
+  errorReports: ErrorReport[] = [];
+
+  constructor(
+    protected _dialogRef: MatDialogRef<ErrorDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) errorReports$: Observable<ErrorReport>
+  ) {
+    errorReports$.subscribe((error) => this.errorReports.push(error));
+  }
+
+  onClose() {
+    this._dialogRef.close();
+  }
+}
